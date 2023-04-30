3a44b5a5a233bdf99343e28556e0311ca30973ab
Sinan Yumurtaci <sinan.yumurtaci@gmail.com>
2023-04-30 15:30:38 -0400

    set up visuals of society meter

--- app/src/ClimateGame.js
+++ app/src/ClimateGame.js
@@ -111,6 +111,25 @@ const Card = ({content}) => {
     )
 }
 
+const meterN = 15;
+
+const societyMeterColorAtX = (x) => {
+    let red = [255, 0, 0];
+    let green = [0, 255, 0];
+
+    let b = (x+1) / meterN;
+    let a = 1 - b;
+    
+    let res = [
+        red[0] * a + green[0] * b,
+        red[1] * a + green[1] * b,
+        red[2] * a + green[2] * b,
+    ]
+
+    return "rgba(" + res[0].toString() + ", " + res[1].toString() + ", " + res[2].toString() + ", 70%)"
+}
+
+const scenario_name = "Easter Island";
 const ClimateGame = () => {
     const [grid, setGrid] = useState(newDefaultTileGrid);
     const [villagers, setVillagers] = useState(5);
@@ -119,6 +138,7 @@ const ClimateGame = () => {
     wood: 20,
     food: 20,
     });
+
 
     const changeVillagerCountOnTile = (tile, delta) => {
         /*
@@ -151,10 +171,33 @@ const ClimateGame = () => {
         
     };
 
+    const advanceTurn = () => {
+
+        let report = getResourceReportOfGrid(grid);
+        let newResources = {...resources};
+        let newGrid = {...grid};
+        let newVillagers = villagers;
+
+        for(let resource in report){
+            newResources[resource] += report[resource];
+        }
+
+        for(let tile_key in newGrid){
+            newVillagers += newGrid[tile_key].villagers;
+            newGrid[tile_key].villagers = 0;
+            newGrid[tile_key].action = null;
+        }
+
+        setVillagers(newVillagers);
+        setResources(newResources);
+        setTurn(turns + 1);
+        setGrid(newGrid);
+    }
+
     let resourceChangeReport = getResourceReportOfGrid(grid);
 
     return (
-    <div style={{display:"flex", flexDirection:"row"}}>
+    <div style={{display:"flex", flexDirection:"row", justifyContent: "space-around"}}>
         <div>
             <div className="tile-grid" style={{width: "1200px", height:"900px"}}>
             {iterateOverGridInRowOrder(grid).map((tile, index) => (
@@ -171,7 +214,7 @@ const ClimateGame = () => {
                                
                             <button style={{marginRight: "6px"}} onClick={() => changeVillagerCountOnTile(tile, -1)}>{tile.villagers > 0 ? "-" : "cancel"}</button>
                             <Meeples count={tile.villagers}/>
-                            <button style={{marginLeft: "6px"}} onClick={() => {changeVillagerCountOnTile(tile, +1); console.log(getResourceReportOfTile(tile))}}>+</button>
+                            <button disabled={villagers === 0} style={{marginLeft: "6px"}} onClick={() => {changeVillagerCountOnTile(tile, +1); console.log(getResourceReportOfTile(tile))}}>+</button>
                         </div>)
                     :
                         (
@@ -196,49 +239,74 @@ const ClimateGame = () => {
             </div>
         </div>
 
-        <div>
+        <div style={{flexGrow: 1, display: "flex", flexDirection: "column"}}>
 
-            <h1>Turn {turns}</h1>
+            <div style={{display: "flex", flexDirection: "row", justifyContent: "space-around"}}>
+                <h1>Scenario: {scenario_name}</h1>
+                <h1>Turn {turns}</h1>
+            </div>
 
-            <p>Unallocated meeples</p><br/>
-            <Meeples count={villagers}/>
+            <div>
+                SOCIETY METER
+                <div style={{display: "flex"}}>
+                {[...Array(meterN)].map((e, i) => <div className="meterBarCell" style={{height: "40px", width: (100/meterN).toString() + "%", backgroundColor: societyMeterColorAtX(i)}} key={i}>AAA</div>)}
 
-            <table>
-                <tr>
-                    <th></th>
-                    <th>Current</th>
-                    <th>Actions</th>
-                    <th>New</th>
-                </tr>
-                <tr>
-                    <td>Food</td>
-                    <td>{resources.food}</td>
-                    <td>{resourceChangeReport["food"]}</td>
-                    <td>{resources.food + resourceChangeReport["food"]}</td>
-                </tr>
-                <tr>
-                    <td>Wood</td>
-                    <td>{resources.wood}</td>
-                    <td>{resourceChangeReport["wood"]}</td>
-                    <td>{resources.wood + resourceChangeReport["wood"]}</td>
-                </tr>
-                
-            </table>
-
-            <h3>Available Actions</h3>
-            <div className="actions" style={{textAlign: "left"}}>
-                {ACTIONS.map((action, index) => (
-                    <div key={index}>
-                    
-                    <p><b>{action.name}</b> {"["+ action.tileTypes.join('] [') + "]"}</p>
-                    </div>
-                ))}
                 </div>
-                <Card></Card>
-            </div> 
-        
+                <div>
+                    {villagers > 0 ?
+                    <p>To allocate: <Meeples count={villagers}/></p> :
+                    <button disabled={villagers !== 0} onClick={() => {advanceTurn()}}>Advance turn</button>
+                    }
+                    <br/>
+                </div>
+            </div>
            
+            
+            <div style={{display:"flex", flexDirection:"row", justifyContent: "space-around"}}>
+                <div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Current</th>
+                                <th>Actions</th>
+                                <th>New</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Food</td>
+                                <td>{resources.food}</td>
+                                <td>{resourceChangeReport["food"]}</td>
+                                <td>{resources.food + resourceChangeReport["food"]}</td>
+                            </tr>
+                            <tr>
+                                <td>Wood</td>
+                                <td>{resources.wood}</td>
+                                <td>{resourceChangeReport["wood"]}</td>
+                                <td>{resources.wood + resourceChangeReport["wood"]}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <h3>Available Actions</h3>
+                    <div className="actions" style={{textAlign: "left"}}>
+                    </div>
+                
+                    {ACTIONS.map((action, index) => (
+                        <div key={index}>
+                        
+                        <p><b>{action.name}</b> {"["+ action.tileTypes.join('] [') + "]"}</p>
+                        </div>
+                    ))}
+                </div>
+                <div>
+                    <h3>Societal Demands</h3>
+                    <Card></Card>
+                </div>
+            </div>
+                
         </div>
+    </div>
     );
 };
 
